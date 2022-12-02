41f8b9d5ca99592434cbb6aacc3ea2b30f91f2c7
cdeimer <cullendeimer@gmail.com>
2022-12-02 16:40:08 -0500

    add some more styling, redesign the home page

--- bahs/urls.py
+++ bahs/urls.py
@@ -3,5 +3,6 @@ from bahs import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("history/", views.history, name="history"),
     path("history/<int:page_id>/", views.page_detail, name="page_detail")
 ]
--- bahs/views.py
+++ bahs/views.py
@@ -12,6 +12,13 @@ def home(request):
     }
     return render(request, 'bahs/index.html', context)
 
+def history(request):
+    page_list = Page.objects.order_by('title')
+    context = {
+        'page_list': page_list,
+    }
+    return render(request, 'bahs/history.html', context)
+
 def page_detail(request, page_id):
     page = get_object_or_404(Page, pk=page_id)
     return render(request, 'bahs/page_detail.html', {'page': page})
